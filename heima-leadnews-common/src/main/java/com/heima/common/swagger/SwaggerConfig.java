94ed1572132e721d7f33614da4e763b40a9ebe9c
甜点专家 <3112779976@qq.com>
2025-04-02 16:16:21 +0800

    swagger整合

--- heima-leadnews-common/src/main/java/com/heima/common/swagger/SwaggerConfig.java
+++ heima-leadnews-common/src/main/java/com/heima/common/swagger/SwaggerConfig.java
@@ -1,10 +1,7 @@
 package com.heima.common.swagger;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -15,27 +12,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableKnife4j
-@Import(BeanValidatorPluginsConfiguration.class)
-public class Swagger2Configuration {
+public class SwaggerConfig {
 
-    @Bean(value = "defaultApi2")
-    public Docket defaultApi2() {
-        Docket docket=new Docket(DocumentationType.SWAGGER_2)
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo())
-                //分组名称
-                .groupName("1.0")
                 .select()
-                //这里指定Controller扫描包路径
+                // 指定controller存放的目录路径
                 .apis(RequestHandlerSelectors.basePackage("com.heima"))
                 .paths(PathSelectors.any())
                 .build();
-        return docket;
     }
+
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
+                // 文档标题
                 .title("黑马头条API文档")
-                .description("黑马头条API文档")
+                // 文档描述
+                .description("黑马头条API文档，详细信息......")
+                // 文档版本
                 .version("1.0")
                 .build();
     }
